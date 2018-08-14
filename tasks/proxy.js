97d3004d49ecc45759f80a96a19ff83ea04fe1e3
Ruben Gonzalez <gonzalezruben@ad.corp.google.com>
2018-08-14 12:33:30 -0500

    role migration

--- tasks/proxy.js
+++ tasks/proxy.js
@@ -38,6 +38,7 @@ module.exports = function(grunt) {
 			var roleEntity = {name: roleName};
 			request.post({url: url, timeout: 10000, pool: separateReqPool, body: roleEntity, json: true}, function (err, resp, body) {
 				if (!err && resp.statusCode < 300) {
+					console.log(roleName);
 					var permissionsTo = {resourcePermission: []};
 					permissionsJson.resourcePermission.forEach(function(per) {
 						if(per.organization === apigee.from.org){
@@ -47,6 +48,7 @@ module.exports = function(grunt) {
 							} else {
 								intPer.path = per.path;
 							}
+							console.log(intPer.path);
 							intPer.permissions = per.permissions;
 							permissionsTo.resourcePermission.push(intPer);
 						}
